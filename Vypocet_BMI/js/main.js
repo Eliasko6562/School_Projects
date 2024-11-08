f62b771172b8844912ea0397a0e2423524e5b3b9
Eliasko <eliascomastantine@gmail.com>
2024-11-08 11:33:49 +0100

    added Bmi categories

--- Vypocet_BMI/js/main.js
+++ Vypocet_BMI/js/main.js
@@ -4,19 +4,23 @@ let person = {
     age: 17,
     weight: 70,
     height: 170,
+
     cmToMetres: function(cm) {
-        return cm / 100;
+        return Number.isInteger(cm) ? cm / 100: false;
     },
     bmi: function(w = this.weight, h = this.height) {
         return (w / Math.pow(this.cmToMetres(h),2)).toFixed(2); 
     },
+
     state: function() {
         let bmi = this.bmi();
-        let bmiCategory = bmiCategories.find(obj => obj.age === this.age);
-        if (bmi < bmiCategory.weight[0]) return 'underweight';
-        if ((bmi >= bmiCategory.weight[0]) && (bmi < bmiCategory.weight[1])) return 'optimal weight';
-        if ((bmi >= bmiCategory.weight[1]) && (bmi < bmiCategory.weight[2])) return 'overweight';
-        if ((bmi >= bmiCategory.weight[2]) && (bmi < bmiCategory.weight[3])) return 'obesity';
+        if (bmi < 18.5) return 'underweight';
+        if ((bmi >= 18.5) && (bmi < 25)) return 'optimal weight';
+        if ((bmi >= 25) && (bmi < 30)) return 'overweight';
+        if ((bmi >= 30 ) && (bmi < 40)) return 'obesity';
         return 'severe obesity';
     }
 }
+
+console.log(person.bmi());
+console.log(person.state());
