dcd2162a38078a9dbef23496b306653a608c1744
Elias Constantine <147802118+Eliasko6562@users.noreply.github.com>
2024-12-05 18:10:15 +0000

    fixed error animation and completed Project

--- Formular_JS/scripts/main.js
+++ Formular_JS/scripts/main.js
@@ -11,17 +11,25 @@ const distance = document.getElementById('distance');
 const time = document.getElementById('time');
 const result = document.getElementById('result');
 const errorTime = document.getElementById('errorTime');
+const calculate = document.getElementById('calculate');
 
-
-document.getElementById('calculate').addEventListener('click', function() {
+calculate.addEventListener('click', function() {
     if (time.value < 1){
-        errorTime.innerHTML = ('Cas musi byt vetsi nez 0');
-        document.getElementById('time').style.animation = 'shake 0.2s ease-in-out 0s 2';
+        errorTime.innerHTML = ('Čas musí být větší než 0');
+        document.getElementById('time').style.border = '1px solid red';
+        document.getElementById('time').style.animation = 'none';
+        setTimeout(() => {
+            document.getElementById('time').style.animation = 'shake 0.2s ease-in-out 0s 2';
+        }, 0);
         return;
     }
+    if (time.value <= 1){
+        errorTime.innerHTML = ('');
+        document.getElementById('time').style.border = 'none';
+    }
+
     objekt.distance = distance.value;
     objekt.time = time.value;
 
     result.innerHTML = objekt.speed();
 });
-
