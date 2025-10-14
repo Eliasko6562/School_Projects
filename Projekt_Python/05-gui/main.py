39ee7eaa62bf962b9445204a0b19f0ca8c1d299b
Elias Constantine <147802118+Eliasko6562@users.noreply.github.com>
2025-10-14 13:59:27 +0200

    Add files via upload

--- Projekt_Python/05-gui/main.py
+++ Projekt_Python/05-gui/main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import random as rand
+from tkinter import simpledialog  as sd
+from tkinter import colorchooser as cc
 
 class App: 
     def __init__(self):
@@ -25,7 +27,7 @@ class App:
         
         m_entertainment = tk.Menu(menubar, tearoff=False)
         m_entertainment.add_command(label="Magické Čáry", command=lambda: self.magic_barcode(25, 100))
-        m_entertainment.add_command(label="Šachovnice", command=lambda: self.set_bg("green"))
+        m_entertainment.add_command(label="Šachovnice", command=lambda: self.chessboard_dialog())
         m_entertainment.add_command(label="Terč", command=lambda: self.set_bg("blue"))
         menubar.add_cascade(label="Entertainment", menu=m_entertainment)
         
@@ -43,6 +45,28 @@ class App:
             color = f"#{rand.randint(0x0, 0xffffff):06x}"
             self.canvas.create_line(x, 0, x, h, fill=color, width=width)
             x += width
+            
+    def chessboard(self, size: int = 8, color1: str="black", color2: str="white") -> None:
+        self.reset()
+        self.canvas.config(bg="white")
+        w, h = self._canvas_size()
+        cell_x = w / size
+        cell_y = h / size
+        for y in range(size):
+            for x in range(size):
+                color = color1 if (x + y) % 2 == 0 else color2
+                self.canvas.create_rectangle(x * cell_x, y * cell_y, x * cell_x + cell_x,y * cell_y + cell_y, fill=color)
+            
+    def chessboard_dialog(self):
+        size = sd.askinteger(title="Šachovnice",
+                            prompt="Zadej počet polí",
+                            minvalue=2,
+                            maxvalue=20,
+                            initialvalue=8,
+                            parent=self.root)
+        color1 = cc.askcolor(title="Vyber pozadí")[1]
+        color2 = cc.askcolor(title="Vyber barvu")[1]
+        self.chessboard(size=size, color1=color1, color2=color2)
         
     def set_bg(self, color: str) -> None:
         self.canvas.config(background=color)   
